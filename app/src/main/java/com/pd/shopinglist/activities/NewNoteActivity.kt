378cf090182a60cfb4d1f0fce23d2ba63751752a
Pushin <geiby@mail.ru>
2022-05-03 17:34:20 +0300

    11. Создаем NewNoteActivity.

--- app/src/main/java/com/pd/shopinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/pd/shopinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,14 @@
 package com.pd.shopinglist.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.pd.shopinglist.R
 import com.pd.shopinglist.databinding.ActivityNewNoteBinding
+import com.pd.shopinglist.fragments.NoteFragment.Companion.DESCRIPTION_KEY
+import com.pd.shopinglist.fragments.NoteFragment.Companion.TITLE_KEY
 
 class NewNoteActivity : AppCompatActivity() {
 
@@ -23,16 +26,32 @@ class NewNoteActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.id_save) {
-            finish()
+        if (item.itemId == R.id.id_save) { // при нажатии на кнопку мы передаем результат
+            setMainResult()
         } else if (item.itemId == android.R.id.home) {
             finish()
         }
         return super.onOptionsItemSelected(item)
 
     }
-    private fun actionBarSettings(){//подключаем кнопку назад в actionBar
-        val ab=supportActionBar
+
+    private fun setMainResult() {
+        val intent = Intent().apply {
+            putExtra(
+                TITLE_KEY,
+                binding.edTitle.toString()
+            )//взяли текст из вью и поместили в наш интент
+            putExtra(
+                DESCRIPTION_KEY,
+                binding.edDescription.toString()
+            )//взяли текст из вью и поместили в наш интент
+        }
+        setResult(RESULT_OK,intent) // отправляем результат
+        finish() //закрываем активити после передачи данных
+    }
+
+    private fun actionBarSettings() {//подключаем кнопку назад в actionBar
+        val ab = supportActionBar
         ab?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/com/pd/shopinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/pd/shopinglist/fragments/NoteFragment.kt
@@ -1,11 +1,16 @@
 package com.pd.shopinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import com.pd.shopinglist.R
 import com.pd.shopinglist.activities.MainApp
@@ -16,16 +21,20 @@ import com.pd.shopinglist.db.MainViewModel
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>//для того чтобы ждать результата
+
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onCkickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java)) // запускаем интент
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onEditResult()
 
     }
 
@@ -37,7 +46,22 @@ class NoteFragment : BaseFragment() {
         return binding.root
     }
 
+    private fun onEditResult() {
+        // инициализируем editLauncher , а также ждем результата от запущенной активити
+        editLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                Log.d("MyLog", "title: ${it.data?.getStringExtra(TITLE_KEY)} ")
+                Log.d("MyLog", "description: ${it.data?.getStringExtra(DESCRIPTION_KEY)} ")
+            }
+        }
+    }
+
     companion object {
+        const val TITLE_KEY = "title_key"
+        const val DESCRIPTION_KEY = "description_key"
+
         @JvmStatic
         fun newInstance() = NoteFragment()
     }
